97441216ad411c9176e401a13fca4d85d3dc9e7e
DarkMiata <samvermeulen@gmail.com>
2017-01-18 11:47:22 +0100

    ajout: - header incluant css, bootstrap dans les fichiers front - répertoire images avec banniere - répertoire css avec style.css (base) et style_register.css (css d'un formulaire perso) - page de connexion - répertoire inc (includes) - répertoire js (javascript bootstrap)

--- header.inc.php
+++ header.inc.php
@@ -1,7 +1,7 @@
 <html>
   <head>
 <meta charset="UTF-8">
-    <link href="style.css" rel="stylesheet" type="text/css"/>
+<!--    <link href="css\style.css" rel="stylesheet" type="text/css"/>-->
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
   </head>
   
